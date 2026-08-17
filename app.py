311789eb1ead108e33735d78d821c65cc64c3d91
Vaishnavi-Ghotekar <vaishnavighotekar@gmail.com>
2026-08-17 23:20:57 +0530

    Configure Flask app to use environment variables
    Added environment variable configuration for Flask app.

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import os
+
 app = Flask(__name__)
 
 
@@ -10,3 +12,10 @@ def hello_world():
 @app.route('/health')
 def health():
     return 'Server is up and running'
+
+
+if __name__ == '__main__':
+    debug_mode = os.environ.get('FLASK_DEBUG', 'false').lower() == 'true'
+    host = os.environ.get('FLASK_HOST', '0.0.0.0')  # nosec B104
+    port = int(os.environ.get('FLASK_PORT', '80'))
+    app.run(debug=debug_mode, host=host, port=port)
